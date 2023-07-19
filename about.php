362348a40f255dd0d20f1b2e23c1adc28e40d13d
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 23:02:01 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,28 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>About</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About - ABP Chat Application</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
-        <h1>About</h1>
+        <h1>About ABP Chat Application</h1>
     </header>
 
     <nav>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about.php">About</a></li>
-            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
 
     <main>
-        <h2>About Us</h2>
-        <p>We are a team of passionate individuals dedicated to creating amazing websites.</p>
+        <h2>About ABP Chat Application</h2>
+        <p>
+            The ABP Chat Application is a simple chat application built using PHP and JavaScript. It allows users to
+            engage in real-time conversations and exchange messages with each other.
+        </p>
+        <p>
+            Features of the ABP Chat Application include:
+        </p>
+        <ul>
+            <li>Real-time messaging using WebSocket technology</li>
+            <li>User-friendly interface</li>
+            <li>Ability to send and receive messages instantly</li>
+            <li>Responsive design for optimal viewing on different devices</li>
+        </ul>
     </main>
 
     <footer>
-        <p>&copy; <?php echo date('Y'); ?> My Static Website. All rights reserved.</p>
+        <p>&copy; <?php echo date("Y"); ?> ABP Chat Application. All rights reserved.</p>
     </footer>
 </body>
+
 </html>
