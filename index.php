1b0743854cb3619002844c185419f757cf484dfe
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 22:39:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
-echo '<h1>This is coming from code pipeline</h>';
+// Handle form submissions
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
 
+    // Perform any necessary processing or validation
 
-echo '<h2> changes in code...by Akshay Pawar </h2>';
+    // Display a success message
+    $message = "Thank you, $name! Your email ($email) has been submitted successfully.";
+}
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample Form</title>
+</head>
+<body>
+    <h1>Sample Form</h1>
+
+    <?php if (isset($message)): ?>
+        <p><?php echo $message; ?></p>
+    <?php else: ?>
+        <form method="POST" action="">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required><br>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br>
+
+            <button type="submit">Submit</button>
+        </form>
+    <?php endif; ?>
+</body>
+</html>
