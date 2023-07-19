517488b5589a8f145b7d876baff11a8ea9c689f7
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 23:11:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My Static Website</title>
-</head>
-<body>
-    <header>
-        <h1>Welcome to My Static Website</h1>
-    </header>
+ <?php
 
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about.php">About</a></li>
-            <li><a href="/contact.php">Contact</a></li>
-        </ul>
-    </nav>
+echo '<h1>This is coming from code pipeline</h>';
 
-    <main>
-        <h2>Home</h2>
-        <p>This is the homepage of my static website.</p>
-    </main>
 
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> My Static Website. All rights reserved.</p>
-    </footer>
-</body>
-</html>
+echo '<h2> changes in code...by Akshay Pawar </h2>';
