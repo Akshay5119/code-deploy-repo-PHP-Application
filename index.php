e2faa7a05a5d7b6020ee73a821d3eb3716f8a166
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 22:52:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,28 @@
-<?php
-
-// Handle form submissions
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-
-    // Perform any necessary processing or validation
-
-    // Display a success message
-    $message = "Thank you, $name! Your email ($email) has been submitted successfully.";
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sample Form</title>
+    <title>My Static Website</title>
 </head>
 <body>
-    <h1>Sample Form</h1>
+    <header>
+        <h1>Welcome to My Static Website</h1>
+    </header>
 
-    <?php if (isset($message)): ?>
-        <p><?php echo $message; ?></p>
-    <?php else: ?>
-        <form method="POST" action="">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required><br>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </nav>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required><br>
+    <main>
+        <h2>Home</h2>
+        <p>This is the homepage of my static website.</p>
+    </main>
 
-            <button type="submit">Submit</button>
-        </form>
-    <?php endif; ?>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> My Static Website. All rights reserved.</p>
+    </footer>
 </body>
 </html>
