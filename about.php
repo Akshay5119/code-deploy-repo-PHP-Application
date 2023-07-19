c87e66f5140cf358e4a828efa05f2f93320295b3
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 22:57:17 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <header>
+        <h1>About</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h2>About Us</h2>
+        <p>We are a team of passionate individuals dedicated to creating amazing websites.</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> My Static Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
