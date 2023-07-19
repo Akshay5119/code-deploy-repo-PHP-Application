0a3233e7cf4bcaa26302fd8892bb61ce640ea3d0
Akshay5119 <90967825+Akshay5119@users.noreply.github.com>
2023-07-19 22:57:54 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+<body>
+    <header>
+        <h1>Contact</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h2>Contact Us</h2>
+        <p>Feel free to reach out to us using the contact details provided below:</p>
+        <ul>
+            <li>Email: info@example.com</li>
+            <li>Phone: +1 123-456-7890</li>
+        </ul>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> My Static Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
